a1dedb2894a522952d2edc5b8ced33857095d033
higyedcode <Higyed12@gmail.com>
2024-05-24 18:48:59 +0300

    modified backend api url

--- src/api.tsx
+++ src/api.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // Set your base URL
+    // baseURL: 'http://localhost:8080/api', // Set your base URL
     //baseURL: 'https://mppeventmanagerfreedb.onrender.com/api', // Set your base URL
+    baseURL: 'https://mppeventsmanagerbackend.onrender.com/api',
 
     timeout: 5000, // Set timeout
     headers: {
